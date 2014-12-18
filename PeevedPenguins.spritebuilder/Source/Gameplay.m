4c20dacb287659e8e8a95c12031bc1baae98c1bf
Asirisss <asirisss87@gmail.com>
2014-12-19 00:56:14 +0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -54,6 +54,13 @@
     }
 }
 
+- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // whenever touches move, update the position of the mouseJointNode to the touch position
+    CGPoint touchLocation = [touch locationInNode:_contentNode];
+    _mouseJointNode.position = touchLocation;
+}
+
 
 -(void)launchPenguin {
     //loads the penguin.cbb we have set up in Splitebuilder
